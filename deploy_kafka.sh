2c6b73bdd92e46c1ba790c6bfc69b034eb9abaa7
TattiQ <tatyana.mva@gmail.com>
2017-08-13 11:57:50 +0300

    added topics

--- deploy_kafka.sh
+++ deploy_kafka.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 yum install docker mailx git -y
+echo '{"bip": "192.168.111.1/24", "insecure-registries": ["supdockerhub:5000"]}' >> /etc/docker/daemon.json
 systemctl start docker
 systemctl enable docker
 
@@ -18,11 +19,21 @@ ip=`ip a | grep eno | grep inet | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | h
 
 docker run -d -p 9092:9092 --net=host --env KAFKA_ADVERTISED_HOST_NAME=$ip --env ZOOKEEPER_IP=$ip --name kafka kafka_11
 
-docker run --rm kafka_11 kafka-topics.sh --create --topic bundle_event --replication-factor 1 --partitions 10 --zookeeper $ip:2181 --config cleanup.policy=compact
-docker run --rm kafka_11 kafka-topics.sh --create --topic sf_case_event --replication-factor 1 --partitions 10 --zookeeper $ip:2181 --config cleanup.policy=compact
+docker run --rm kafka_11 kafka-topics.sh --create --topic bundle_events --replication-factor 1 --partitions 20 --zookeeper $ip:2181 --config cleanup.policy=compact
 
-docker run --rm kafka_11 kafka-topics.sh --create --topic pirate_report --replication-factor 1 --partitions 10 --zookeeper $ip:2181 --config cleanup.policy=compact
+docker run --rm kafka_11 kafka-topics.sh --create --topic report_pirates --replication-factor 1 --partitions 1 --zookeeper $ip:2181
 
-docker run --rm kafka_11 kafka-topics.sh --create --topic bundle_queued --replication-factor 1 --partitions 10 --zookeeper $ip:2181 --config cleanup.policy=compact
+docker run --rm kafka_11 kafka-topics.sh --create --topic bundle_queued --replication-factor 1 --partitions 20 --zookeeper $ip:2181
 
-echo $ip | mail -s "Kafka and zookeeper containers deployed" tatyana.koroleva@fakedomain.com
+docker run --rm kafka_11 kafka-topics.sh --create --topic sf_dashboard_query_results --replication-factor 1 --partitions 1 --zookeeper $ip:2181 --config cleanup.policy=compact
+
+docker run --rm kafka_11 kafka-topics.sh --create --topic logs_bug_matching --replication-factor 1 --partitions 1 --zookeeper $ip:2181
+docker run --rm kafka_11 kafka-topics.sh --create --topic logs_downloader --replication-factor 1 --partitions 1 --zookeeper $ip:2181
+
+docker run --rm kafka_11 kafka-topics.sh --create --topic sf_account_query_requests --replication-factor 1 --partitions 10 --zookeeper $ip:2181
+
+docker run --rm kafka_11 kafka-topics.sh --create --topic sf_account_query_results --replication-factor 1 --partitions 1 --zookeeper $ip:2181 --config cleanup.policy=compact
+
+docker run --rm kafka_11 kafka-topics.sh --create --topic report_bug_found --replication-factor 1 --partitions 20 --zookeeper $ip:2181
+
+echo $ip | mail -s "Kafka and zookeeper containers deployed" tatyana.mva@gmail.com
