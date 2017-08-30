ac5786e1a16a594833446f8205d0d8a705273150
Tatyana Koroleva <tatyana.mva@gmail.com>
2017-08-30 17:39:02 +0300

    Update deploy_kafka.sh

--- deploy_kafka.sh
+++ deploy_kafka.sh
@@ -16,7 +16,7 @@ docker run -d -p 2181:2181 --net=host --name zookeeper jplock/zookeeper
 cd /tmp && git clone https://github.com/TattiQ/dockerized_kafka.git
 cd dockerized_kafka && docker build -t kafka_11 .
 echo "Getting ip"
-ip=`ip a | grep ens | grep inet | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1`
+ip=`hostname -I | cut -d " " -f1`
 
 docker run -d -p 9092:9092 --net=host --env KAFKA_ADVERTISED_HOST_NAME=$ip --env ZOOKEEPER_IP=$ip --name kafka kafka_11
 
@@ -24,7 +24,8 @@ echo "Creating topics"
 sleep 30
 
 docker run --rm kafka_11 kafka-topics.sh --create --topic bundle_events --replication-factor 1 --partitions 20 --zookeeper $ip:2181 --config cleanup.policy=compact
-
+docker run --rm kafka_11 kafka-topics.sh --create --topic logs_sf_comments --replication-factor 1 --partitions 1 --zookeeper $ip:2181
+docker run --rm kafka_11 kafka-topics.sh --create --topic sf_case_event --replication-factor 1 --partitions 10 --zookeeper $ip:2181
 docker run --rm kafka_11 kafka-topics.sh --create --topic report_pirates --replication-factor 1 --partitions 1 --zookeeper $ip:2181
 
 docker run --rm kafka_11 kafka-topics.sh --create --topic bundle_queued --replication-factor 1 --partitions 20 --zookeeper $ip:2181
